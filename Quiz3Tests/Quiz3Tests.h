293b8a3d53d51203e764e8703725a7997528c9b3
jondiehl <diehl@myrtle.cs.moravian.edu>
2014-02-28 04:17:18 -0500

    initial

---
+++ Quiz3Tests/Quiz3Tests.h
@@ -0,0 +1,13 @@
+//
+//  Quiz3Tests.h
+//  Quiz3Tests
+//
+//  Created by Jon Diehl on 1/31/14.
+//  Copyright (c) 2014 Jon Diehl. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Quiz3Tests : SenTestCase
+
+@end
---
+++ Quiz3Tests/Quiz3Tests.m
@@ -0,0 +1,32 @@
+//
+//  Quiz3Tests.m
+//  Quiz3Tests
+//
+//  Created by Jon Diehl on 1/31/14.
+//  Copyright (c) 2014 Jon Diehl. All rights reserved.
+//
+
+#import "Quiz3Tests.h"
+
+@implementation Quiz3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Quiz3Tests");
+}
+
+@end
